ceaee0bc93b05d72acd843f396a9a3ebbc53a136
Thom <tphtiger@yahoo.com>
2017-01-30 20:54:56 -0500

    updated albumctrl, fixtures.js, collection, index, and landing to include Fixtures as a Service

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,9 @@
+ (function() {
+     function AlbumCtrl(Fixtures) {
+         this.albumData = Fixtures.getAlbum();
+     }
+
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+ })();
--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,5 +1,9 @@
 (function() {
-    function CollectionCtrl() {
+    function CollectionCtrl() {     
+        this.albums = [];
+        for (var i=0; i < 12; i++) {
+            this.albums.push(angular.copy(albumPicasso));
+        }  
     }
  
     angular
