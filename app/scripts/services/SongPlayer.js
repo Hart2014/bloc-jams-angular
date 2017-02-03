a982796fc81540a469c7be78b037f30a0c32e6ec
Thom <tphtiger@yahoo.com>
2017-02-02 21:34:31 -0500

    updated the SongPlayer service with setSong private function with directive reflections within the album.html template to allow for song changes

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,45 @@
+(function() {
+    function SongPlayer() {
+        var SongPlayer = {};
+        
+        var currentSong = null;
+        var currentBuzzObject = null; 
+        
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+ 
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+ 
+        currentSong = song;
+    };
+        
+        SongPlayer.play = function(song) {
+            if (currentSong !== song){
+                setSong(song);
+                currentBuzzObject.play();
+                song.playing = true;
+            } else if (currentSong === song){
+                if (currentBuzzObject.isPaused()){
+                    currentBuzzObject.play();
+                }
+            }
+        };
+        
+        SongPlayer.pause = function(song){
+            currentBuzzObject.pause();
+            song.playing = false;
+        };
+        
+        return SongPlayer;
+    }
+ 
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+})();
