bd091873337bc26ee13f2a70b5a9a4fcad094332
Thom <tphtiger@yahoo.com>
2017-02-04 18:39:52 -0500

    updated SongPlayer.js with playerbar integration and functions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,30 @@
 (function() {
-    function config($stateProvider, $locationProvider){
-         $locationProvider
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
             .html5Mode({
-                enabled: true, 
+                enabled: true,
                 requireBase: false
-         });
-        
+            });
+
         $stateProvider
-            .state({
+            .state('landing', {
                 url: '/',
                 controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
-        })
+            })
             .state('album', {
                 url: '/album',
+                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
-        })
+            })
             .state('collection', {
                 url: '/collection',
                 controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
-        });
-     }
-     
+            });
+    };
+
     angular
         .module('blocJams', ['ui.router'])
         .config(config);
- })();
+})();
